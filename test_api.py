1340dd43281a5ac9a885dbe55f434fae9476fd0c
githubofakash <118022166+githubofakash@users.noreply.github.com>
2022-11-12 02:41:18 +0530

    add environment variables to securely store data
    add environment variables to securely store and access users credentials

--- test_api.py
+++ test_api.py
@@ -1,5 +1,6 @@
 from api_helper import ShoonyaApiPy
 import logging
+from dotenv import load_dotenv
  
 #enable dbug to see request and responses
 logging.basicConfig(level=logging.DEBUG)
@@ -7,12 +8,16 @@ logging.basicConfig(level=logging.DEBUG)
 #start of our program
 api = ShoonyaApiPy()
 
-#credentials
-user    = <uid>
-pwd     = <password>
+#create .env file to securely access user credentials in program
+load_dotenv('/home/akash/Documents/Projects/.env')
+
+#os.getenv() will fetch values from .env file 
+#credentials 
+uid     = os.getenv('userid')
+pwd     = os.getenv('password')
 factor2 = <2nd factor>
-vc      = <vendor code>
-app_key = <secret key>
+vc      = os.getenv('vendor_key')
+app_key = os.getenv('api_key')
 imei    = <imei>
 
 #make the api call
