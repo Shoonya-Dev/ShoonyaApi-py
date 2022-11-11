619bd6a8a2ef43504b3c8810efd8f62bae40e40d
githubofakash <118022166+githubofakash@users.noreply.github.com>
2022-11-12 02:53:31 +0530

    correct variable assignment

--- test_api.py
+++ test_api.py
@@ -19,10 +19,10 @@ otp = pyotp.TOTP(token).now()
 #credentials 
 uid     = os.getenv('userid')
 pwd     = os.getenv('password')
-factor2 = os.getenv('totp')
-vc      = otp
+vc      = os.getenv('vendor_code')
 app_key = os.getenv('api_key')
 imei    = <imei>
+factor2 = otp
 
 #make the api call
 ret = api.login(userid=uid, password=pwd, twoFA=factor2, vendor_code=vc, api_secret=app_key, imei=imei)
